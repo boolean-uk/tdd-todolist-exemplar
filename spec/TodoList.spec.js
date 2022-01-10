2c98ad57e2ebf48332a00f3d8e642feca56ee444
phillipdunn <phillipdunn@hotmail.com>
2022-01-10 16:48:20 +0000

    Add - multiple todo test

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -19,4 +19,22 @@ describe('TodoList', () => {
     // verify
     expect(result).toEqual(expected);
   });
+
+  it('create multiple todo items', () => {
+    // set up
+    const todoOne = {
+      id: 1,
+      text: 'turn the heating on!',
+      status: 'incomplete',
+    };
+    const todoTwo = {
+      id: 2,
+      text: 'make a cuppa',
+      status: 'incomplete',
+    };
+    // execute
+    todoList.create(todoOne.text, todoOne.id);
+    const result = todoList.create(todoTwo.text, todoTwo.id);
+    expect(result).toEqual(todoTwo);
+  });
 });
