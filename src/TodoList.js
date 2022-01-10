6e3477bdd1d9447a1fcca3fc3d345f4a29f481f9
phillipdunn <phillipdunn@hotmail.com>
2022-01-10 16:47:10 +0000

    Add - todo list logic

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,12 @@
 class TodoList {
-
+  create(text) {
+    let expected = {
+      id: 1,
+      text: text,
+      status: 'incomplete',
+    };
+    return expected;
+  }
 }
 
-module.exports = TodoList
+module.exports = TodoList;
