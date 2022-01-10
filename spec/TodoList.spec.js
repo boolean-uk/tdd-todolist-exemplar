bc3692233e946452491aadb1b967867a74dccd1e
Lewis <ljoncb@gmail.com>
2022-01-10 16:41:59 +0000

    Can create multiple todos

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -19,4 +19,22 @@ describe("TodoList", () => {
     // verify
     expect(result).toEqual(expected)
   })
+  it("create multiple todo items", () => {
+    // set up
+    const first = {
+      id: 1,
+      text: 'do something',
+      status: 'incomplete'
+    }
+    const second = {
+      id: 2,
+      text: 'do something else',
+      status: 'incomplete'
+    }
+    // execute
+    todoList.create(first.text)
+    const result = todoList.create(second.text)
+    // verify
+    expect(result).toEqual(second)
+  })
 })
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,19 @@
 class TodoList {
 
+    constructor() {
+        this.todos = []
+    }
+
+    create(task) {
+        const todo = {
+            text: task,
+            status: "incomplete"
+        }
+        todo.id = this.todos.length + 1
+        this.todos.push(todo)
+        return todo
+    }
+
 }
 
 module.exports = TodoList
